fb24ad5dd7c74c7048a21c8a36a4498c69210b25
sudhakardalai <sudhakardalai768@gmail.com>
2022-12-28 18:28:47 +0530

    Login Signup Added

--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,20 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const config = require('config');
+const auth = require("../middleware/auth");
 const { check, validationResult } = require('express-validator');
+const jwtSecret = process.env.jwtSecret
 
 const User = require('../models/User');
 
-// @route     POST api/users
-// @desc      Regiter a user
-// @access    Public
+// Routes for signup
+/**
+ * @method - POST
+ * @param - /signup
+ * @description - User SignUp
+ */
 router.post(
-  '/',
+  '/signup',
   check('name', 'Please add name').not().isEmpty(),
   check('email', 'Please include a valid email').isEmail(),
   check(
@@ -53,7 +57,7 @@ router.post(
 
       jwt.sign(
         payload,
-        config.get('jwtSecret'),
+        jwtSecret,
         {
           expiresIn: 360000
         },
@@ -69,4 +73,73 @@ router.post(
   }
 );
 
+/**
+ * @method - POST
+ * @param - /login
+ * @description - User Login
+ */
+
+router.post(
+  '/login',
+  
+  check('email', 'Please include a valid email').isEmail(),
+  check(
+    'password',
+    'Please enter a password with 6 or more characters'
+  ).isLength({ min: 6 }),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: "Username not found" });
+      }
+      const checkPassword = await bcrypt.compare(password, user.password);
+            
+            if(!checkPassword)
+                return res.status(400).json({
+                    message: "Incorrect Password"
+                });
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        jwtSecret,
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+router.get("/me", auth, async (req, res) => {
+  try {
+    // request.user is getting fetched from the auth function defined in middleware
+    // after token authentication
+    const user = await User.findById(req.user.id);
+    res.json(user);
+  } catch (e) {
+    res.send({ message: "Error in Fetching user" });
+  }
+});
 module.exports = router;
